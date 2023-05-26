b448feb3f06839cb917de078379fb005ba9bd0df
paulo_junior_ext <paulo_junior_ext@carrefour.com>
2023-05-25 23:48:38 -0400

    fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -14,9 +14,10 @@ sudo dnf install snapd
 sudo dnf install vim
 sudo dnf install htop
 
-##Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dnf install ./google-chrome-stable_current_amd64.deb
+##chrome
+sudo dnf install fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install google-chrome-stable -y
 
 ##HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -35,24 +36,22 @@ brew install argocd
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
-##Cloud SDK
-sudo dnf install dnf-transport-https ca-certificates gnupg
+##gcloud install
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo dnf install google-cloud-cli
 
-cd ~
-mkdir gcp-sdk && cd gcp-sdk
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-425.0.0-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-425.0.0-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
-gcloud components install cloud_sql_proxy docker-credential-gcr kustomize beta kubectl
-
-##Code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/dnf/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/dnf/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/dnf/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-sudo dnf install dnf-transport-https
-sudo dnf update
+##vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
 sudo dnf install code
 
 
@@ -74,19 +73,7 @@ sudo dnf update
 sudo dnf install --install-recommends winehq-stable
 wine --version
 
-
-##Docker
-#curl -fsSL https://get.docker.com | bash
-
-#Download the following dependencies:
-sudo dnf-get install dnf-transport-https ca-certificates curl gnupg2 software-properties-common
-
-
-cd ~/Downloads/
-wget -O https://www.playonlinux.com/script_files/PlayOnLinux/4.3.4/PlayOnLinux_4.3.4.deb
-sudo dnf install ./PlayOnLinux_4.3.4.deb
-
-# Minikube
+##Minikube
 sudo dnf install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
 sudo adduser -a $USER libvirt
 sudo adduser -a $USER libvirt-qemu
@@ -95,57 +82,38 @@ newgrp libvirt-qemu
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-#Brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-dnf-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-dnf-release.s3.brave.com/ stable main"|sudo tee /etc/dnf/sources.list.d/brave-browser-release.list
-sudo dnf update
+#brave
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser
 
-# Spotify
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/dnf/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/dnf/sources.list.d/spotify.list
-sudo dnf-get update && sudo dnf-get install spotify-client -y
+##spotify
+sudo dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub com.spotify.Client
 
-# Authy
+#Authy
 sudo snap install authy
 
-# Remmina
+#Remmina
 sudo snap install remmina
 
-# Terminator
+#Terminator
 sudo dnf install terminator
 
-# Postman
+#Postman
 sudo snap install postman
 
-# Notion
+#Notion
 sudo snap install notion-snap-reborn
 
-# Pipewire - para compartilhamento de tela no meet
-sudo dnf install pipewire -y
-systemctl --user start pipewire wireplumber
+##lens
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install kontena-lens --classic
 
-# Lens
-curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/dnf/debian stable main" | sudo tee /etc/dnf/sources.list.d/lens.list > /dev/null
-sudo dnf update
-sudo dnf install lens
-
-#Download Docker’s official GPG key to verify the integrity of packages
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo dnf-key add -
-
-#Add the Docker repository to your system repository
-sudo add-dnf-repository "deb [arch=amd64] https://download.docker.com/linux/debian bookworm stable"
-
-#Update the dnf repository:
-sudo dnf-get update
-
-#Install Docker Engine and containerd:
-sudo dnf-get install docker-ce docker-ce-cli containerd.io
-
-#Check the status by typing:
-systemctl status docker
-docker -v
-
-sudo groupadd docker
-newgrp docker
+#docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker

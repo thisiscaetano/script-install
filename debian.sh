6c1803bcb748b699cae7a8218042f6fea43a7d74
paulo_junior <paulo_junior@carrefour.com>
2024-04-15 11:50:42 -0300

    obsidian install

--- debian.sh
+++ debian.sh
@@ -88,3 +88,6 @@ curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /us
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
 sudo apt update
 sudo apt install lens
+
+# Obsidian
+flatpak install flathub md.obsidian.Obsidian

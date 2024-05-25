4be046af15d9d509eb237cbc66f5959bad720f57
paulo_junior <paulo_junior@carrefour.com>
2024-05-24 23:35:57 -0300

    install konsole

--- debian.sh
+++ debian.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y terminator
+sudo apt install -y konsole
 sudo apt install -y neofetch
 sudo apt install -y curl
 sudo apt-get install -y build-essential
